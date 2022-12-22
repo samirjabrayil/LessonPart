6afdc36ccfdc25182c61035678aa2f4044d8addc
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2022-12-22 18:40:46 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LessonPart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello, Master");
+        }
+    }
+}
